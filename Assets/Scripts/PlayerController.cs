a9b11359b917abdd21d5896ecbd0ef9c347e3447
Victor Doktare <victor@doktare.com>
2021-03-23 13:36:51 +0100

    Jump rework

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 [RequireComponent(typeof(Rigidbody))]
 [RequireComponent(typeof(CapsuleCollider))]
 public class PlayerController : MonoBehaviour
-{   
+{
+    #region Variables
     //Editable character stats
     [Header("Character Settings")] 
     [Range(1, 99)]
@@ -18,9 +20,6 @@ public class PlayerController : MonoBehaviour
     [Range(1, 99)]
     [SerializeField] private float playerJumpHeight = 10f;
     
-    //Public
-    [HideInInspector] public Vector3 upAxis;
-
     //Component references
     private Rigidbody playerRigidbody;
     private InputManager inputManager;
@@ -29,10 +28,11 @@ public class PlayerController : MonoBehaviour
     //Movement
     private Quaternion playerRotation;
     private Vector3 playerVelocity;
-    private Vector3 desiredVelocity;
     private Vector3 playerDisplacement;
-    private Vector3 desiredPosition;
+    private Vector3 desiredVelocity;
+    //private Vector3 desiredPosition;
     private float maxSpeedChange;
+    #endregion
 
     void Awake()
     {
@@ -40,17 +40,25 @@ public class PlayerController : MonoBehaviour
         playerCollider = GetComponent<CapsuleCollider>();
         inputManager = FindObjectOfType<InputManager>();
     }
-
-    private void Start()
+    
+    void Update()
     {
-        
+        //Using player input for desired velocity
+        desiredVelocity = new Vector3(inputManager.playerInput.x, 0f, inputManager.playerInput.z) * playerMoveSpeed;
     }
 
     void FixedUpdate()
     {
-        upAxis = -Physics.gravity.normalized;
-            
         PlayerMove();
+    }
+    
+    private void PlayerMove()
+    {
+        //Adjust physics/velocity
+        playerVelocity = playerRigidbody.velocity;
+        maxSpeedChange = playerAcceleration * Time.deltaTime;
+        playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, desiredVelocity.x, maxSpeedChange);
+        playerVelocity.z = Mathf.MoveTowards(playerVelocity.z, desiredVelocity.z, maxSpeedChange);
         
         //Jump Check
         if (inputManager.isJumping)
@@ -58,35 +66,20 @@ public class PlayerController : MonoBehaviour
             inputManager.isJumping = false;
             PlayerJump();
         }
-    }
-
-    private void Update()
-    {
         
-    }
+        //Override Rigidbody and apply adjusted velocity
+        playerRigidbody.velocity = playerVelocity;
 
-    private void PlayerMove()
-    {
-        //Combining acceleration and velocity to acquire smoother movement
-        desiredVelocity = new Vector3(inputManager.playerInput.x, 0f, inputManager.playerInput.z) * playerMoveSpeed;
-        maxSpeedChange = playerAcceleration * Time.deltaTime;
-        playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, desiredVelocity.x, maxSpeedChange);
-        playerVelocity.z = Mathf.MoveTowards(playerVelocity.z, desiredVelocity.z, maxSpeedChange);
-        //Move player to a new position
-        playerDisplacement = playerVelocity * Time.deltaTime;
-        desiredPosition = transform.localPosition += playerDisplacement;
-
-        //Check if player is in motion and rotate player accordingly
-        if (playerVelocity != Vector3.zero)
+        //Rotate if player is moving
+        if (playerVelocity.x != 0f || playerVelocity.z != 0f)
         {
-            //Store current player direction and rotate player accordingly, mathf.pow is used to avoid high numbers when tweaking
-            playerRotation = Quaternion.LookRotation(playerVelocity, upAxis);
+            playerRotation = Quaternion.LookRotation(new Vector3(playerVelocity.x, 0f, playerVelocity.z), Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, playerRotation, Mathf.Pow(playerRotateSpeed,2)*Time.deltaTime);
         }
     }
 
     private void PlayerJump()
     {
-        
+        playerVelocity.y += playerJumpHeight;
     }
 }
