9e5ec5efa5140b363065b4cdd7dfe129c91e0f4d
Victor Doktare <victor@doktare.com>
2021-03-23 15:37:32 +0100

    Slope controll

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,35 +10,54 @@ public class PlayerController : MonoBehaviour
 {
     #region Variables
     //Editable character stats
-    [Header("Character Settings")] 
-    [Range(1, 99)]
+    [Header("Character Movement Settings")] 
+    [Range(0, 99)][Tooltip("How fast player moves in a direction")]
     [SerializeField] private float playerMoveSpeed = 10f;
-    [Range(1, 99)]
+    [Range(0, 99)][Tooltip("How fast player accelerate when moving.")]
     [SerializeField] private float playerAcceleration = 10f;
-    [Range (1, 99)]
+    [Range (0, 99)][Tooltip("How fast player rotates towards movement direction.")]
     [SerializeField] private float playerRotateSpeed = 10f;
-    [Range(1, 99)]
+    [Range(0, 5)][Tooltip("Number of Air Jumps allowed.")]
+    [SerializeField] private int playerAirJumps = 0;
+    [Range(1, 99)][Tooltip("Height in meters.")]
     [SerializeField] private float playerJumpHeight = 10f;
+    [Range(0, 99)][Tooltip("How fast player accelerate while in air.")]
+    [SerializeField] private float playerAirAcceleration = 1f;
+    
+    [Header("Ground Settings")] 
+    [Range(0, 90)][Tooltip("Max angle on what is considered ground")]
+    [SerializeField] private float maxGroundAngle = 25f;
     
     //Component references
     private Rigidbody playerRigidbody;
     private InputManager inputManager;
-    private CapsuleCollider playerCollider;
+    //private CapsuleCollider playerCollider;
 
     //Movement
     private Quaternion playerRotation;
     private Vector3 playerVelocity;
     private Vector3 playerDisplacement;
     private Vector3 desiredVelocity;
-    //private Vector3 desiredPosition;
     private float maxSpeedChange;
+    
+    //Misc
+    private int jumpCount;
+    private bool onGround;
+    private float minGroundDotProduct;
+    
     #endregion
 
+    private void OnValidate()
+    {
+        minGroundDotProduct = Mathf.Cos(maxGroundAngle * Mathf.Deg2Rad);
+    }
+    
     void Awake()
     {
         playerRigidbody = GetComponent<Rigidbody>();
-        playerCollider = GetComponent<CapsuleCollider>();
         inputManager = FindObjectOfType<InputManager>();
+        OnValidate();
+        //playerCollider = GetComponent<CapsuleCollider>();
     }
     
     void Update()
@@ -49,14 +68,16 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        UpdateState();
+        AdjustVelocity();
         PlayerMove();
     }
-    
-    private void PlayerMove()
+
+    private void AdjustVelocity()
     {
-        //Adjust physics/velocity
-        playerVelocity = playerRigidbody.velocity;
-        maxSpeedChange = playerAcceleration * Time.deltaTime;
+        //Adjust velocity & use different acceleration settings bases on if grounded or not
+        var acceleration = onGround ? playerAcceleration : playerAirAcceleration;
+        maxSpeedChange = acceleration * Time.deltaTime;
         playerVelocity.x = Mathf.MoveTowards(playerVelocity.x, desiredVelocity.x, maxSpeedChange);
         playerVelocity.z = Mathf.MoveTowards(playerVelocity.z, desiredVelocity.z, maxSpeedChange);
         
@@ -67,19 +88,61 @@ public class PlayerController : MonoBehaviour
             PlayerJump();
         }
         
-        //Override Rigidbody and apply adjusted velocity
-        playerRigidbody.velocity = playerVelocity;
+        onGround = false;
+    }
 
+    private void UpdateState()
+    {
+        playerVelocity = playerRigidbody.velocity;
+        
+        if (onGround)
+        {
+            jumpCount = 0;
+        }
+    }
+
+    private void PlayerMove()
+    {
         //Rotate if player is moving
         if (playerVelocity.x != 0f || playerVelocity.z != 0f)
         {
             playerRotation = Quaternion.LookRotation(new Vector3(playerVelocity.x, 0f, playerVelocity.z), Vector3.up);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, playerRotation, Mathf.Pow(playerRotateSpeed,2)*Time.deltaTime);
         }
+        
+        //Move Player
+        playerRigidbody.velocity = playerVelocity;
     }
 
     private void PlayerJump()
     {
-        playerVelocity.y += playerJumpHeight;
+        if (onGround || jumpCount < playerAirJumps)
+        {
+            jumpCount++;
+            //Want jump height do be consistent and unaffected by downwards forces, so resetting velocity.Y before jumping
+            playerVelocity.y = 0f;
+            //This allows me to get a more accurate jump height. For example, a playerJumpHeight of 1 is very close to 1 meter in game.
+            playerVelocity.y += Mathf.Sqrt(-2 * Physics.gravity.y * playerJumpHeight);   
+        }
+    }
+
+    //Ground check based on normal direction of collided object.
+    void EvaluateCollision(Collision collision)
+    {
+        for (int i = 0; i < collision.contactCount; i++)
+        {
+            Vector3 contactNormal = collision.GetContact(i).normal;
+            onGround |= contactNormal.y >= minGroundDotProduct;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        EvaluateCollision(collision);
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        EvaluateCollision(collision);
     }
 }
