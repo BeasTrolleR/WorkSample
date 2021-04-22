2e2347071b7cbaa3277fbbbf42f5bb0481b0364d
Victor Doktare <victor@doktare.com>
2021-04-22 20:47:32 +0200

    Prototype Done

--- Assets/CameraManager.cs
+++ Assets/CameraManager.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
+using UnityEngine.SocialPlatforms;
 
 public class CameraManager : MonoBehaviour
 {
     [Header("Camera Settings")]
     [Tooltip("Swaps the camera offset to Positive or Negative")]
     public bool camOffsetChange = false;
+    
     private CinemachineTransposer offsetChange;
     
+    private float offsetValue = 10f;
+    [Range(0f, 1f)]
+    private float offsetLerpAmount = 1f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,15 +28,35 @@ public class CameraManager : MonoBehaviour
         CamOffset();
     }
     
+    //Interpolates the cam offset vector to give a smooth cam transition when changing gravity.
     private void CamOffset()
     {
+        Vector3 posOffset = new Vector3(0f, offsetValue, -10f);
+        Vector3 negOffset = new Vector3(0f, -offsetValue, -10f);
+
         if (camOffsetChange == true)
         {
-            offsetChange.m_FollowOffset = new Vector3(0f, -10f, -10f);;
+            if (offsetLerpAmount <= 0f)
+            {
+                offsetLerpAmount = 0f;
+            }
+            else if (offsetLerpAmount <= 1f)
+            {
+                offsetLerpAmount -= Time.deltaTime;
+            }
         }
         else
         {
-            offsetChange.m_FollowOffset = new Vector3(0f, 10f, -10f);
+            if (offsetLerpAmount >= 1f)
+            {
+                offsetLerpAmount = 1f;
+            }
+            else if (offsetLerpAmount >= 0f)
+            {
+                offsetLerpAmount += Time.deltaTime;
+            }
         }
+        
+        offsetChange.m_FollowOffset = Vector3.Lerp(negOffset, posOffset, offsetLerpAmount);
     }
 }
