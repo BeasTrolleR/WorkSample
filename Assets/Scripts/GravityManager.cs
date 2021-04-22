7a5cb81251f133d23407e997747e64da221012ca
Victor Doktare <victor@doktare.com>
2021-04-22 17:40:21 +0200

    Gravity Manager

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -6,31 +6,30 @@ using UnityEngine.PlayerLoop;
 
 public class GravityManager : MonoBehaviour
 {
-    public bool gravityOn = false;
-    private Vector3 gravityDirection;
+    [Header("General Gravity Settings")] 
+    [Range(0, 20)][Tooltip("How strong the gravity pulls")]
+    [SerializeField] float gravityStrength = 9.81f;
+    public bool changeGravityDirection = false;
 
     private void Start()
     {
-
     }
 
     private void Update()
     {
         GravitySwap();
+
     }
 
     private void GravitySwap()
     {
-        gravityDirection = Physics.gravity;
-        Debug.Log(gravityOn);
-        if (gravityOn == false)
+        if (changeGravityDirection == false)
         {
-            gravityDirection = new Vector3(0, -9.81f, 0);
+            Physics.gravity = new Vector3(0f, -gravityStrength, 0f);
         }
         else
         {
-            gravityDirection = new Vector3(0, 9.81f, 0);
+            Physics.gravity = new Vector3(0f, gravityStrength, 0f);
         }
-
     }
 }
